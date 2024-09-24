b48a055b0548a037d73ff9c4dda2851419eafaec
LucientZ <koblenzpoisiden@gmail.com>
2024-09-24 17:19:28 -0500

    Added title ROM header parsing

--- src/js/globals.js
+++ src/js/globals.js
@@ -2,8 +2,22 @@ const BYTE_VALUES = Object.freeze({
     KB: 1024
 });
 
-const ADDRESSES = Object.freeze({
-    ROM_BANK: 0x000
+const RomHeaderAddresses = Object.freeze({
+    ENTRY: 0x100,
+    LOGO: 0x104,
+    TITLE: 0x134,
+    MANUFACTURER_CODE: 0x13F,
+    CGB_FLAG: 0x143,
+    NEW_LICENSEE_CODE: 0x144,
+    SGB_FLAG: 0x146,
+    CARTRIDGE_TYPE: 0x147,
+    ROM_SIZE: 0x148,
+    RAM_SIZE: 0x149,
+    DESTINATION_CODE: 0x14A,
+    OLD_LICENSEE_CODE: 0x14B,
+    ROM_VERSION: 0x14C,
+    HEADER_CHECKSUM: 0x14D,
+    GLOBAL_CHECKSUM: 0x14E
 });
 
 
@@ -18,6 +32,7 @@ const ADDRESSES = Object.freeze({
  * @type {GlobalVariables}
 */
 const GLOBALS = {
+    GAME_TITLE: "",
     ROM: null,
     RAM: new Uint8Array(),
 }
--- src/js/main.js
+++ src/js/main.js
@@ -20,13 +20,39 @@ async function dropHandler(event) {
                 console.log(file);
                 GLOBALS.ROM = new Uint8Array(await file.arrayBuffer());
             }
+            else{
+                alert("Invalid ROM")
+            }
         }
     }
 
     if (GLOBALS.ROM) {
+        parseROM(GLOBALS.ROM);
     }
 }
 
+/**
+ * 
+ * @param {Uint8Array} rom 
+ */
+function parseROM(rom) {
+    if(rom.length < 0x150){
+        alert("Invalid ROM");
+        return
+    }
+
+    GLOBALS.GAME_TITLE = "";
+    for(let i = RomHeaderAddresses.TITLE; i < RomHeaderAddresses.TITLE + 16; i++){
+        characterCode = GLOBALS.ROM[i];
+        if(characterCode == 0x00){
+            break;
+        }
+        GLOBALS.GAME_TITLE += String.fromCharCode(character);
+    }
+
+    console.log(GLOBALS.GAME_TITLE);
+}
+
 function stringifyROM(rom) {
-    
+
 }

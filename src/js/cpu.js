0bb0b38bafd37873ec9cfebb4f58d1df1c39bf37
LucientZ <koblenzpoisiden@gmail.com>
2024-09-28 13:46:23 -0500

    Skeleton of arithmetic and 16 bit instructions

--- src/js/cpu.js
+++ src/js/cpu.js
@@ -288,6 +288,36 @@ function doLoadInstruction(instruction) {
 }
 
 /**
+ * This handles instructions 0x80-0xBF
+ * @param {number} instruction 
+ */
+function doArithmeticInstruction(instruction) {
+    let value;
+
+    switch (instruction & 0x07) {
+        case 0x0: value = Registers.B; break;
+        case 0x1: value = Registers.C; break;
+        case 0x2: value = Registers.D; break;
+        case 0x3: value = Registers.E; break;
+        case 0x4: value = Registers.H; break;
+        case 0x5: value = Registers.L; break;
+        case 0x6: value = gameboyRead(combineRegisters(Registers.H, Registers.L)); Globals.cycleNumber++; break;
+        case 0x7: value = Registers.A; break;
+    }
+
+    switch ((instruction >> 3) & 0x7) {
+        case 0: // ADD
+        case 1: // ADC
+        case 2: // SUB
+        case 3: // SBC
+        case 4: // AND
+        case 5: // XOR
+        case 6: // OR
+        case 7: // COMPARE
+    }
+}
+
+/**
  * https://meganesu.github.io/generate-gb-opcodes/
  * https://gbdev.io/gb-opcodes/optables/
  */
@@ -722,393 +752,15 @@ const opcodeTable8Bit = {
         Registers.Fh = 0;
         Globals.cycleNumber++;
     },
-    0x40: () => {
-        doLoadInstruction(0x40);
-    },
-    0x41: () => {
-        doLoadInstruction(0x41);
-    },
-    0x42: () => {
-        doLoadInstruction(0x42);
-    },
-    0x43: () => {
-        doLoadInstruction(0x43);
-    },
-    0x44: () => {
-        doLoadInstruction(0x44);
-    },
-    0x45: () => {
-        doLoadInstruction(0x45);
-    },
-    0x46: () => {
-        doLoadInstruction(0x46);
-    },
-    0x47: () => {
-        doLoadInstruction(0x47);
-    },
-    0x48: () => {
-        doLoadInstruction(0x48);
-    },
-    0x49: () => {
-        doLoadInstruction(0x49);
-    },
-    0x4A: () => {
-        doLoadInstruction(0x4A);
-    },
-    0x4B: () => {
-        doLoadInstruction(0x4B);
-    },
-    0x4C: () => {
-        doLoadInstruction(0x4C);
-    },
-    0x4D: () => {
-        doLoadInstruction(0x4D);
-    },
-    0x4E: () => {
-        doLoadInstruction(0x4E);
-    },
-    0x4F: () => {
-        doLoadInstruction(0x4F);
-    },
-    0x50: () => {
-        doLoadInstruction(0x50);
-    },
-    0x51: () => {
-        doLoadInstruction(0x51);
-    },
-    0x52: () => {
-        doLoadInstruction(0x52);
-    },
-    0x53: () => {
-        doLoadInstruction(0x53);
-    },
-    0x54: () => {
-        doLoadInstruction(0x54);
-    },
-    0x55: () => {
-        doLoadInstruction(0x55);
-    },
-    0x56: () => {
-        doLoadInstruction(0x56);
-    },
-    0x57: () => {
-        doLoadInstruction(0x57);
-    },
-    0x58: () => {
-        doLoadInstruction(0x58);
-    },
-    0x59: () => {
-        doLoadInstruction(0x59);
-    },
-    0x5A: () => {
-        doLoadInstruction(0x5A);
-    },
-    0x5B: () => {
-        doLoadInstruction(0x5B);
-    },
-    0x5C: () => {
-        doLoadInstruction(0x5C);
-    },
-    0x5D: () => {
-        doLoadInstruction(0x5D);
-    },
-    0x5E: () => {
-        doLoadInstruction(0x5E);
-    },
-    0x5F: () => {
-        doLoadInstruction(0x5F);
-    },
-    0x60: () => {
-        doLoadInstruction(0x60);
-    },
-    0x61: () => {
-        doLoadInstruction(0x61);
-    },
-    0x62: () => {
-        doLoadInstruction(0x62);
-    },
-    0x63: () => {
-        doLoadInstruction(0x63);
-    },
-    0x64: () => {
-        doLoadInstruction(0x64);
-    },
-    0x65: () => {
-        doLoadInstruction(0x65);
-    },
-    0x66: () => {
-        doLoadInstruction(0x66);
-    },
-    0x67: () => {
-        doLoadInstruction(0x67);
-    },
-    0x68: () => {
-        doLoadInstruction(0x68);
-    },
-    0x69: () => {
-        doLoadInstruction(0x69);
-    },
-    0x6A: () => {
-        doLoadInstruction(0x6A);
-    },
-    0x6B: () => {
-        doLoadInstruction(0x6B);
-    },
-    0x6C: () => {
-        doLoadInstruction(0x6C);
-    },
-    0x6D: () => {
-        doLoadInstruction(0x6D);
-    },
-    0x6E: () => {
-        doLoadInstruction(0x6E);
-    },
-    0x6F: () => {
-        doLoadInstruction(0x6F);
-    },
-    0x70: () => {
-        doLoadInstruction(0x70);
-    },
-    0x71: () => {
-        doLoadInstruction(0x71);
-    },
-    0x72: () => {
-        doLoadInstruction(0x72);
-    },
-    0x73: () => {
-        doLoadInstruction(0x73);
-    },
-    0x74: () => {
-        doLoadInstruction(0x74);
-    },
-    0x75: () => {
-        doLoadInstruction(0x75);
-    },
+    // GAP of Load Instructions 0x40-0x75
     0x76: () => {
         // TODO
         // HALT
         Globals.halted = true;
         Globals.cycleNumber++;
     },
-    0x77: () => {
-        doLoadInstruction(0x77);
-    },
-    0x78: () => {
-        doLoadInstruction(0x78);
-    },
-    0x79: () => {
-        doLoadInstruction(0x79);
-    },
-    0x7A: () => {
-        doLoadInstruction(0x7A);
-    },
-    0x7B: () => {
-        doLoadInstruction(0x7B);
-    },
-    0x7C: () => {
-        doLoadInstruction(0x7C);
-    },
-    0x7D: () => {
-        doLoadInstruction(0x7D);
-    },
-    0x7E: () => {
-        doLoadInstruction(0x7E);
-    },
-    0x7F: () => {
-        doLoadInstruction(0x7F);
-    },
-    0x80: () => {
-
-    },
-    0x81: () => {
-
-    },
-    0x82: () => {
-
-    },
-    0x83: () => {
-
-    },
-    0x84: () => {
-
-    },
-    0x85: () => {
-
-    },
-    0x86: () => {
-
-    },
-    0x87: () => {
-
-    },
-    0x88: () => {
-
-    },
-    0x89: () => {
-
-    },
-    0x8A: () => {
-
-    },
-    0x8B: () => {
-
-    },
-    0x8C: () => {
-
-    },
-    0x8D: () => {
-
-    },
-    0x8E: () => {
-
-    },
-    0x8F: () => {
-
-    },
-    0x90: () => {
-
-    },
-    0x91: () => {
-
-    },
-    0x92: () => {
-
-    },
-    0x93: () => {
-
-    },
-    0x94: () => {
-
-    },
-    0x95: () => {
-
-    },
-    0x96: () => {
-
-    },
-    0x97: () => {
-
-    },
-    0x98: () => {
-
-    },
-    0x99: () => {
-
-    },
-    0x9A: () => {
-
-    },
-    0x9B: () => {
-
-    },
-    0x9C: () => {
-
-    },
-    0x9D: () => {
-
-    },
-    0x9E: () => {
-
-    },
-    0x9F: () => {
-
-    },
-    0xA0: () => {
-
-    },
-    0xA1: () => {
-
-    },
-    0xA2: () => {
-
-    },
-    0xA3: () => {
-
-    },
-    0xA4: () => {
-
-    },
-    0xA5: () => {
-
-    },
-    0xA6: () => {
-
-    },
-    0xA7: () => {
-
-    },
-    0xA8: () => {
-
-    },
-    0xA9: () => {
-
-    },
-    0xAA: () => {
-
-    },
-    0xAB: () => {
-
-    },
-    0xAC: () => {
-
-    },
-    0xAD: () => {
-
-    },
-    0xAE: () => {
-
-    },
-    0xAF: () => {
-
-    },
-    0xB0: () => {
-
-    },
-    0xB1: () => {
-
-    },
-    0xB2: () => {
-
-    },
-    0xB3: () => {
-
-    },
-    0xB4: () => {
-
-    },
-    0xB5: () => {
-
-    },
-    0xB6: () => {
-
-    },
-    0xB7: () => {
-
-    },
-    0xB8: () => {
-
-    },
-    0xB9: () => {
-
-    },
-    0xBA: () => {
-
-    },
-    0xBB: () => {
-
-    },
-    0xBC: () => {
-
-    },
-    0xBD: () => {
-
-    },
-    0xBE: () => {
-
-    },
-    0xBF: () => {
-
-    },
+    // GAP of Load Instructions 0x77-0x7F
+    // GAP of Arithmetic instructions 0x80-0xBF
     0xC0: () => {
 
     },
@@ -1303,779 +955,101 @@ const opcodeTable8Bit = {
     },
 }
 
-const opcodeTable16Bit = {
-    0x00: () => {
-
-    },
-    0x01: () => {
-    },
-    0x02: () => {
-    },
-    0x03: () => {
-
-    },
-    0x04: () => {
-
-    },
-    0x05: () => {
-
-    },
-    0x06: () => {
-
-    },
-    0x07: () => {
-
-    },
-    0x08: () => {
-
-    },
-    0x09: () => {
-
-    },
-    0x0A: () => {
-
-    },
-    0x0B: () => {
-
-    },
-    0x0C: () => {
-
-    },
-    0x0D: () => {
-
-    },
-    0x0E: () => {
-
-    },
-    0x0F: () => {
-
-    },
-    0x10: () => {
-
-    },
-    0x11: () => {
-
-    },
-    0x12: () => {
-
-    },
-    0x13: () => {
-
-    },
-    0x14: () => {
-
-    },
-    0x15: () => {
-
-    },
-    0x16: () => {
-
-    },
-    0x17: () => {
-
-    },
-    0x18: () => {
-
-    },
-    0x19: () => {
-
-    },
-    0x1A: () => {
-
-    },
-    0x1B: () => {
-
-    },
-    0x1C: () => {
-
-    },
-    0x1D: () => {
-
-    },
-    0x1E: () => {
-
-    },
-    0x1F: () => {
-
-    },
-    0x20: () => {
-
-    },
-    0x21: () => {
-
-    },
-    0x22: () => {
-
-    },
-    0x23: () => {
-
-    },
-    0x24: () => {
-
-    },
-    0x25: () => {
-
-    },
-    0x26: () => {
-
-    },
-    0x27: () => {
-
-    },
-    0x28: () => {
-
-    },
-    0x29: () => {
-
-    },
-    0x2A: () => {
-
-    },
-    0x2B: () => {
-
-    },
-    0x2C: () => {
-
-    },
-    0x2D: () => {
-
-    },
-    0x2E: () => {
-
-    },
-    0x2F: () => {
-
-    },
-    0x30: () => {
-
-    },
-    0x31: () => {
-
-    },
-    0x32: () => {
-
-    },
-    0x33: () => {
-
-    },
-    0x34: () => {
-
-    },
-    0x35: () => {
-
-    },
-    0x36: () => {
-
-    },
-    0x37: () => {
-
-    },
-    0x38: () => {
-
-    },
-    0x39: () => {
-
-    },
-    0x3A: () => {
-
-    },
-    0x3B: () => {
-
-    },
-    0x3C: () => {
-
-    },
-    0x3D: () => {
-
-    },
-    0x3E: () => {
-
-    },
-    0x3F: () => {
-
-    },
-    0x40: () => {
-
-    },
-    0x41: () => {
-
-    },
-    0x42: () => {
-
-    },
-    0x43: () => {
-
-    },
-    0x44: () => {
-
-    },
-    0x45: () => {
-
-    },
-    0x46: () => {
-
-    },
-    0x47: () => {
-
-    },
-    0x48: () => {
-
-    },
-    0x49: () => {
-
-    },
-    0x4A: () => {
-
-    },
-    0x4B: () => {
-
-    },
-    0x4C: () => {
-
-    },
-    0x4D: () => {
-
-    },
-    0x4E: () => {
-
-    },
-    0x4F: () => {
-
-    },
-    0x50: () => {
-
-    },
-    0x51: () => {
-
-    },
-    0x52: () => {
-
-    },
-    0x53: () => {
-
-    },
-    0x54: () => {
-
-    },
-    0x55: () => {
-
-    },
-    0x56: () => {
-
-    },
-    0x57: () => {
-
-    },
-    0x58: () => {
-
-    },
-    0x59: () => {
-
-    },
-    0x5A: () => {
-
-    },
-    0x5B: () => {
-
-    },
-    0x5C: () => {
-
-    },
-    0x5D: () => {
-
-    },
-    0x5E: () => {
-
-    },
-    0x5F: () => {
-
-    },
-    0x60: () => {
-
-    },
-    0x61: () => {
-
-    },
-    0x62: () => {
-
-    },
-    0x63: () => {
-
-    },
-    0x64: () => {
-
-    },
-    0x65: () => {
-
-    },
-    0x66: () => {
-
-    },
-    0x67: () => {
-
-    },
-    0x68: () => {
-
-    },
-    0x69: () => {
-
-    },
-    0x6A: () => {
-
-    },
-    0x6B: () => {
-
-    },
-    0x6C: () => {
-
-    },
-    0x6D: () => {
-
-    },
-    0x6E: () => {
-
-    },
-    0x6F: () => {
-
-    },
-    0x70: () => {
-
-    },
-    0x71: () => {
-
-    },
-    0x72: () => {
-
-    },
-    0x73: () => {
-
-    },
-    0x74: () => {
-
-    },
-    0x75: () => {
-
-    },
-    0x76: () => {
-
-    },
-    0x77: () => {
-
-    },
-    0x78: () => {
-
-    },
-    0x79: () => {
-
-    },
-    0x7A: () => {
-
-    },
-    0x7B: () => {
-
-    },
-    0x7C: () => {
-
-    },
-    0x7D: () => {
-
-    },
-    0x7E: () => {
-
-    },
-    0x7F: () => {
-
-    },
-    0x80: () => {
-
-    },
-    0x81: () => {
-
-    },
-    0x82: () => {
-
-    },
-    0x83: () => {
-
-    },
-    0x84: () => {
-
-    },
-    0x85: () => {
-
-    },
-    0x86: () => {
-
-    },
-    0x87: () => {
-
-    },
-    0x88: () => {
-
-    },
-    0x89: () => {
-
-    },
-    0x8A: () => {
-
-    },
-    0x8B: () => {
-
-    },
-    0x8C: () => {
-
-    },
-    0x8D: () => {
-
-    },
-    0x8E: () => {
-
-    },
-    0x8F: () => {
-
-    },
-    0x90: () => {
-
-    },
-    0x91: () => {
-
-    },
-    0x92: () => {
-
-    },
-    0x93: () => {
-
-    },
-    0x94: () => {
-
-    },
-    0x95: () => {
-
-    },
-    0x96: () => {
-
-    },
-    0x97: () => {
-
-    },
-    0x98: () => {
-
-    },
-    0x99: () => {
-
-    },
-    0x9A: () => {
-
-    },
-    0x9B: () => {
-
-    },
-    0x9C: () => {
-
-    },
-    0x9D: () => {
-
-    },
-    0x9E: () => {
-
-    },
-    0x9F: () => {
-
-    },
-    0xA0: () => {
-
-    },
-    0xA1: () => {
-
-    },
-    0xA2: () => {
-
-    },
-    0xA3: () => {
-
-    },
-    0xA4: () => {
-
-    },
-    0xA5: () => {
-
-    },
-    0xA6: () => {
-
-    },
-    0xA7: () => {
-
-    },
-    0xA8: () => {
-
-    },
-    0xA9: () => {
-
-    },
-    0xAA: () => {
-
-    },
-    0xAB: () => {
-
-    },
-    0xAC: () => {
-
-    },
-    0xAD: () => {
-
-    },
-    0xAE: () => {
-
-    },
-    0xAF: () => {
-
-    },
-    0xB0: () => {
-
-    },
-    0xB1: () => {
-
-    },
-    0xB2: () => {
-
-    },
-    0xB3: () => {
-
-    },
-    0xB4: () => {
-
-    },
-    0xB5: () => {
-
-    },
-    0xB6: () => {
-
-    },
-    0xB7: () => {
-
-    },
-    0xB8: () => {
-
-    },
-    0xB9: () => {
-
-    },
-    0xBA: () => {
-
-    },
-    0xBB: () => {
-
-    },
-    0xBC: () => {
-
-    },
-    0xBD: () => {
-
-    },
-    0xBE: () => {
-
-    },
-    0xBF: () => {
-
-    },
-    0xC0: () => {
-
-    },
-    0xC1: () => {
-
-    },
-    0xC2: () => {
-
-    },
-    0xC3: () => {
-
-    },
-    0xC4: () => {
-
-    },
-    0xC5: () => {
-
-    },
-    0xC6: () => {
-
-    },
-    0xC7: () => {
-
-    },
-    0xC8: () => {
-
-    },
-    0xC9: () => {
-
-    },
-    0xCA: () => {
-
-    },
-    0xCB: () => {
-
-    },
-    0xCC: () => {
-
-    },
-    0xCD: () => {
-
-    },
-    0xCE: () => {
-
-    },
-    0xCF: () => {
-
-    },
-    0xD0: () => {
-
-    },
-    0xD1: () => {
-
-    },
-    0xD2: () => {
-
-    },
-    0xD3: () => {
-
-    },
-    0xD4: () => {
-
-    },
-    0xD5: () => {
-
-    },
-    0xD6: () => {
-
-    },
-    0xD7: () => {
-
-    },
-    0xD8: () => {
-
-    },
-    0xD9: () => {
-
-    },
-    0xDA: () => {
-
-    },
-    0xDB: () => {
-
-    },
-    0xDC: () => {
-
-    },
-    0xDD: () => {
-
-    },
-    0xDE: () => {
-
-    },
-    0xDF: () => {
-
-    },
-    0xE0: () => {
-
-    },
-    0xE1: () => {
-
-    },
-    0xE2: () => {
-
-    },
-    0xE3: () => {
-
-    },
-    0xE4: () => {
-
-    },
-    0xE5: () => {
-
-    },
-    0xE6: () => {
-
-    },
-    0xE7: () => {
-
-    },
-    0xE8: () => {
-
-    },
-    0xE9: () => {
-
-    },
-    0xEA: () => {
-
-    },
-    0xEB: () => {
-
-    },
-    0xEC: () => {
-
-    },
-    0xED: () => {
-
-    },
-    0xEE: () => {
-
-    },
-    0xEF: () => {
-
-    },
-    0xF0: () => {
-
-    },
-    0xF1: () => {
-
-    },
-    0xF2: () => {
-
-    },
-    0xF3: () => {
-
-    },
-    0xF4: () => {
-
-    },
-    0xF5: () => {
-
-    },
-    0xF6: () => {
-
-    },
-    0xF7: () => {
-
-    },
-    0xF8: () => {
-
-    },
-    0xF9: () => {
-
-    },
-    0xFA: () => {
-
-    },
-    0xFB: () => {
-
-    },
-    0xFC: () => {
-
-    },
-    0xFD: () => {
-
-    },
-    0xFE: () => {
-
-    },
-    0xFF: () => {
-
-    },
+function doNext16BitInstruction() {
+    const instruction = gameboyRead(Registers.PC++);
+    opcodeTable16Bit[instruction]();
+
+    let value;
+
+    switch (instruction & 0x07) {
+        case 0x0: value = Registers.B; break;
+        case 0x1: value = Registers.C; break;
+        case 0x2: value = Registers.D; break;
+        case 0x3: value = Registers.E; break;
+        case 0x4: value = Registers.H; break;
+        case 0x5: value = Registers.L; break;
+        case 0x6: value = gameboyRead(combineRegisters(Registers.H, Registers.L)); Globals.cycleNumber++; break;
+        case 0x7: value = Registers.A; break;
+    }
+
+    switch (instruction >> 3) {
+        case 0x00:
+            break;
+        case 0x01:
+            break;
+        case 0x02:
+            break;
+        case 0x03:
+            break;
+        case 0x04:
+            break;
+        case 0x05:
+            break;
+        case 0x06:
+            break;
+        case 0x07:
+            break;
+        case 0x08:
+            break;
+        case 0x09:
+            break;
+        case 0x0A:
+            break;
+        case 0x0B:
+            break;
+        case 0x0C:
+            break;
+        case 0x0D:
+            break;
+        case 0x0E:
+            break;
+        case 0x0F:
+            break;
+        case 0x10:
+            break;
+        case 0x11:
+            break;
+        case 0x12:
+            break;
+        case 0x13:
+            break;
+        case 0x14:
+            break;
+        case 0x15:
+            break;
+        case 0x16:
+            break;
+        case 0x17:
+            break;
+        case 0x18:
+            break;
+        case 0x19:
+            break;
+        case 0x1A:
+            break;
+        case 0x1B:
+            break;
+        case 0x1C:
+            break;
+        case 0x1D:
+            break;
+        case 0x1E:
+            break;
+        case 0x1F:
+            break;
+    }
 }
 
-function doNextInstruction() {
-    instruction = gameboyRead(Registers.PC++);
+function doNext8BitInstruction() {
+    const instruction = gameboyRead(Registers.PC++);
     if (instruction === 0xCB) {
-        opcodeTable16Bit[gameboyRead(Registers.PC++)]();
+        doNext16BitInstruction();
+    }
+    else if (instruction >= 0x40 && instruction <= 0x75 || instruction >= 0x77 && instruction <= 0x7F) {
+        doLoadInstruction(instruction);
+    }
+    else if (instruction >= 0x80 && instruction <= 0xBF) {
+        doArithmeticInstruction(instruction);
     }
     else {
         opcodeTable8Bit[instruction]();

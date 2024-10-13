347cd340748ba02a0e78c8f0995b4c386bd86699
LucientZ <koblenzpoisiden@gmail.com>
2024-10-12 21:23:16 -0500

    Window transparency fix for GBC

--- src/js/io.js
+++ src/js/io.js
@@ -262,7 +262,7 @@ function drawLCDLine(line) {
                         color = pixel;
                     }
 
-                    const priority = Globals.metadata.supportsColor ? ((tileAttributes & 0x80) !== 0 || (IORegisters.LCDC & 0x01)) && pixel !== 0 : true;
+                    const priority = Globals.metadata.supportsColor ? ((tileAttributes & 0x80) !== 0 || (IORegisters.LCDC & 0x01)) : true;
                     renderPixel((IORegisters.WX - 7) + i * 8 + j, line, color, priority, false, palette);
                 }
 
